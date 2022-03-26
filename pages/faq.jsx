2f58f2ae760206916b3a0a7b332f4eb266880928
Roberto B Costa <romeromatallica@gmail.com>
2022-03-26 12:36:38 -0300

    feat: finish the project

---
+++ pages/faq.jsx
@@ -0,0 +1,50 @@
+import Link from "../src/components/Link";
+import Head from "next/head";
+import PageTitle from "../src/components/PageTitle";
+
+export async function getStaticProps(context) {
+  const FAQ_API_URL =
+    "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json";
+
+  const faq = await fetch(FAQ_API_URL)
+    .then((repostaDoServidor) => {
+      return repostaDoServidor.json();
+    })
+    .then((resposta) => {
+      return resposta;
+    });
+
+  return {
+    props: {
+      faq,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default function Faq({ faq }) {
+  console.log(faq);
+  // const [faq, setFaq] = useState([]);
+  // setFaq(dataFaq);
+  // useEffect(() => {}, []);
+
+  return (
+    <div>
+       <PageTitle>FAQ - Alura Cases Campanha</PageTitle>
+      <Link href="/" passHref>
+        Ir Para Home Page
+      </Link>
+      <ul>
+        {faq.map(({ question, answer, index }) => {
+          return (
+            <li key={index}>
+              <article>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </article>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,10 +1,12 @@
+import Head from "next/head";
 import Footer from "../../components/patterns/Footer";
 import Link from "../../components/Link";
 import { theme } from "../../theme/theme";
 import { Image, Box, Text, Icon, Input, Button } from "../../theme/components";
+import PageTitle from "../../components/PageTitle";
 
-const LOGO_ALURA_URL = "http://placehold.it/94x44";
-const SIDE_IMAGE_URL = "http://placehold.it/2878x1640";
+const LOGO_ALURA_URL = "/images/alura-logo.svg";
+const SIDE_IMAGE_URL = "/images/alura-cases.png";
 
 function SideImage() {
   return (
@@ -96,6 +98,7 @@ function SideImage() {
 export default function HomeScreen() {
   return (
     <Box>
+      <PageTitle>Home - Alura Cases Campanha</PageTitle>
       <Box
         as="main"
         styleSheet={{
